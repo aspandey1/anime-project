e1b38861c2ed3af52de1a4cb30938f754a201457
Ashish Pandey <ashish.apandey@hotmail.com>
2023-07-25 20:28:04 -0500

    Ready to deploy to heroku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
@@ -13,6 +14,18 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/users", require("./routes/usersRoutes"));
 app.use("/api/users", require("./routes/libraryRoutes"));
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
